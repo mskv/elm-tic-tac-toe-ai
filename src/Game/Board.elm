8d52602292b9a3d3bc56b5dca1419af9143ef027
Michal Podworny <michal.podworny@monterail.com>
2016-12-02 23:07:53 +0100

    Allow to reduce board with indices

--- src/Game/Board.elm
+++ src/Game/Board.elm
@@ -2,7 +2,7 @@ module Game.Board exposing (..)
 
 import Game.Player exposing (Team(..))
 
-import Array exposing (Array)
+import List
 
 type Field
   = Empty
@@ -10,14 +10,34 @@ type Field
   | Circle
 
 type alias Board =
-  Array (Array Field)
+  List (List Field)
 
 initFiveByFive : Board
 initFiveByFive =
-  Array.fromList
-    [ Array.fromList [ Empty, Empty, Empty, Empty, Empty ]
-    , Array.fromList [ Empty, Empty, Empty, Empty, Empty ]
-    , Array.fromList [ Empty, Empty, Empty, Empty, Empty ]
-    , Array.fromList [ Empty, Empty, Empty, Empty, Empty ]
-    , Array.fromList [ Empty, Empty, Empty, Empty, Empty ]
+    [ [ Empty, Empty, Empty, Empty, Empty ]
+    , [ Empty, Empty, Empty, Empty, Empty ]
+    , [ Empty, Empty, Empty, Empty, Empty ]
+    , [ Empty, Empty, Empty, Empty, Empty ]
+    , [ Empty, Empty, Empty, Empty, Empty ]
     ]
+
+map : (Int -> Int -> Field -> Field) -> Board -> Board
+map fn board =
+  board |>
+    List.indexedMap (\rowIndex row ->
+      row |>
+        List.indexedMap (\colIndex field -> fn rowIndex colIndex field))
+
+reduce : (Int -> Int -> Field -> a -> a) -> a -> Board -> a
+reduce fn initAcc board =
+  List.foldl
+    (\(row, rowIndex) accRow ->
+      List.foldl (\(field, colIndex) acc -> fn rowIndex colIndex field acc)
+        accRow
+        (listWithIndices row))
+    initAcc
+    (listWithIndices board)
+
+listWithIndices : List a -> List (a, Int)
+listWithIndices list =
+  List.map2 (,) list (List.range 0 ((List.length list) - 1))
--- src/Game/BoardRenderer.elm
+++ src/Game/BoardRenderer.elm
@@ -4,11 +4,18 @@ import Html exposing (Html)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
+import Game.Board as Board
 import Game.Board exposing (Board)
+import Game.Board exposing (Field(..))
 
 type Msg
   = NoOp
 
 render : Board -> Html Msg
 render board =
-  Svg.rect [ width "50", height "100" ] []
+  --Svg.g [] (Board.map drawField board)
+  Svg.g [] []
+
+drawField : Int -> Int -> Field -> Html Msg
+drawField rowIndex colIndex field =
+  Svg.rect [ x (toString colIndex), y (toString rowIndex), width "50", height "100" ] []
